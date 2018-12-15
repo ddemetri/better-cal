2927b0684d8de50dcc120bda7eb8a24a657553f4
Daniel Demetri <ddemetri@statestitle.com>
2018-12-15 15:04:03 -0800

    Clean up styles and filename

--- urltoken.py
+++ urltoken.py
@@ -15,7 +15,7 @@ class TokenGenerator:
 		self.user_id = user_id
 
 	def get_token(self):
-		secrets_filepath = os.path.join(os.path.dirname(__file__), 'token_secrets.json')
+		secrets_filepath = os.path.join(os.path.dirname(__file__), 'urltoken_secrets.json')
 		with open(secrets_filepath, 'r') as fp:
 			secrets = json.load(fp)
 		token = hashlib.sha256(self.user_id.encode() + secrets.get('hash_seed').encode()).hexdigest()
